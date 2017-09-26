be245fc45b96c4b2a8157cfd5090f71ce9054417
Luis Javier Palacio Mesa <ljpalaciom@eafit.edu.co>
2017-09-26 11:34:40 -0500

    Eliminando ciclo interno del ejercicio en linea

--- laboratorios/lab03/codigo/Laboratorio3/src/laboratorio3/EjercicioEnLinea.java
+++ laboratorios/lab03/codigo/Laboratorio3/src/laboratorio3/EjercicioEnLinea.java
@@ -5,37 +5,42 @@ import java.util.*;
 public class EjercicioEnLinea {
 
     /**
-     * Este metodo mejorado aprovecha el rapido acceso de un string y la rapida inserción de una lista enlazada
+     * Este metodo mejora la legilibilidad del codigo e imprime la respuesta en O(n)
      */
+   
     public static void linea() {
         LinkedList<Character> list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese una linea:");
         String str = sc.nextLine();
+        boolean inicio = false;
+        int index = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '[') {
-                int j = 1;
-                while (i + j < str.length()) {
-                    if (str.charAt(j + i) == '[' || str.charAt(j + i) == ']') {
-                        break;
-                    } else if (str.charAt(j + i) != '[') {
-                        list.add(j - 1, str.charAt(j + i));
-                    }
-                    j++;
-                }
-                i += j - 1;
-            } else if (str.charAt(i) != ']') {
-                list.add(str.charAt(i));
+            switch (str.charAt(i)) {
+                case '[':
+                    inicio = true;
+                    break;
+                case ']':
+                    inicio = false;
+                    index = 0;
+                    break;
+                default:
+                    if (!inicio) {
+                        list.add(str.charAt(i));
+                    } else {
+                        list.add(index++, str.charAt(i));
+                    }   break;
             }
         }
-        String imprimir = "";
+
         while (!list.isEmpty()) {
-            imprimir += list.remove();
+            System.out.print(list.remove());
         }
-        System.out.println(imprimir);
+
     }
 
     public static void main(String[] args) {
+        System.out.println("Ingrese una linea");
         linea();
     }
+
 }
