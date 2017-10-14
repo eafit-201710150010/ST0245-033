dfc43c142d8f6cfe6a88e74596ef15f3e4210ade
Luis Javier Palacio Mesa <ljpalaciom@eafit.edu.co>
2017-10-13 19:51:58 -0500

    Atendiendo consejo de usar la clase StringBuilder y BufferedReader, ahora UVa acepta la solucion

--- laboratorios/lab03/ejercicioEnLinea/EjercicioEnLinea.java
+++ laboratorios/lab03/ejercicioEnLinea/EjercicioEnLinea.java
@@ -1,45 +1,43 @@
 package laboratorio3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class EjercicioEnLinea {
 
-    /**
-     * Este metodo mejora la legilibilidad del codigo e imprime la respuesta en
-     * O(n)
-     */
-    public static void linea() {
-        LinkedList<Character> list = new LinkedList<>();
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        boolean inicio = false;
-        int index = 0;
-        for (int i = 0; i < str.length(); i++) {
-            switch (str.charAt(i)) {
-                case '[':
-                    inicio = true;
-                    index = 0;
-                    break;
-                case ']':
-                    inicio = false;
-                    index = 0;
-                    break;
-                default:
-                    if (!inicio) {
-                        list.add(str.charAt(i));
-                    } else {
-                        list.add(index++, str.charAt(i));
-                    }
-                    break;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = in.readLine()) != null) {
+            LinkedList<Character> list = new LinkedList<>();
+            boolean inicio = false;
+            int index = 0;
+            for (int i = 0; i < str.length(); i++) {
+                switch (str.charAt(i)) {
+                    case '[':
+                        inicio = true;
+                        index = 0;
+                        break;
+                    case ']':
+                        inicio = false;
+                        index = 0;
+                        break;
+                    default:
+                        if (!inicio) {
+                            list.add(str.charAt(i));
+                        } else {
+                            list.add(index++, str.charAt(i));
+                        }
+                        break;
+                }
             }
-        }
-        while (!list.isEmpty()) {
-            System.out.print(list.remove());
+            StringBuilder s = new StringBuilder();
+            while (!list.isEmpty()) {
+                s.append(list.remove());
+            }
+            System.out.println(s.toString());
         }
     }
-
-    public static void main(String[] args) {
-        linea();
-    }
-
 }
